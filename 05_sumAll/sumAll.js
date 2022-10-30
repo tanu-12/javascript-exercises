6c0adf1c563167961a87fe10c5ea6708b61b8160
Tanu <tanupandey291099@gmail.com>
2022-10-30 16:17:48 +0530

    easier than I expected

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(lower,upper) {
+    let max=upper;
+    let min=lower;
+    let sum=0;
+    if(typeof(lower)!="number"||typeof(upper)!="number"||lower<0||upper<0)
+    return "ERROR";
+    if(lower>upper)
+    {max=lower;
+    min=upper;}
+    for(let i=max;i>=min;i--)
+    sum+=i;
+
+    return sum;
 
 };
 
